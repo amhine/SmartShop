21169b0d014ffc7d1803e536166b0bdaf834b423
amhine <nihadamhine@gmail.com>
2025-11-25 15:16:49 +0100

    SMAR-25 Gérer les erreurs : mauvais mot de passe / utilisateur introuvable.

---
+++ src/main/java/com/microtech/SmartShop/exception/AuthException.java
@@ -0,0 +1,7 @@
+package com.microtech.SmartShop.exception;
+
+public class AuthException extends RuntimeException {
+    public AuthException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/microtech/SmartShop/service/impl/AuthServiceImpl.java
+++ src/main/java/com/microtech/SmartShop/service/impl/AuthServiceImpl.java
@@ -2,6 +2,7 @@ package com.microtech.SmartShop.service.impl;
 
 import com.microtech.SmartShop.dto.UserDTO;
 import com.microtech.SmartShop.entity.User;
+import com.microtech.SmartShop.exception.AuthException;
 import com.microtech.SmartShop.mapper.UserMapper;
 import com.microtech.SmartShop.repository.UserRepository;
 import com.microtech.SmartShop.service.AuthService;
@@ -28,13 +29,13 @@ public class AuthServiceImpl implements AuthService {
     public UserDTO login(UserDTO loginDTO , HttpSession session){
         Optional<User> userOpt = userRepository.findByUsername(loginDTO.getUsername());
         if(userOpt.isEmpty()){
-            throw new RuntimeException("utilisateur non trouve");
+            throw new AuthException("utilisateur non trouve");
         }
         User user = userOpt.get();
         if (!passwordEncoder.matches(loginDTO.getPassword(), user.getPassword())) {
-            throw new RuntimeException("Mot de passe incorrect");
+            throw new AuthException("Mot de passe incorrect");
         }
-        session.setAttribute("user", user);
+        session.setAttribute("USER_ID", user.getId());
         return userMapper.toDto(user);
     }
 
