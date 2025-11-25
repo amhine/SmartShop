fb370703588d6fccccaea00b4e6cf1fe71ccf7a8
amhine <nihadamhine@gmail.com>
2025-11-25 12:25:07 +0100

    SMAR-18 Mettre en place un LoginController.

--- src/main/java/com/microtech/SmartShop/dto/UserDTO.java
+++ src/main/java/com/microtech/SmartShop/dto/UserDTO.java
@@ -1,4 +1,12 @@
 package com.microtech.SmartShop.dto;
 
+import com.microtech.SmartShop.entity.enums.Role;
+import lombok.Data;
+
+@Data
 public class UserDTO {
+    private Long id;
+    private String username;
+    private String password;
+    private Role role;
 }
