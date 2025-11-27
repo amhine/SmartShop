d5ade70a9ec5d51b545e2cbecf9a84b0fb4e5aaa
amhine <nihadamhine@gmail.com>
2025-11-27 22:03:30 +0100

    SMAR-36 Implémenter ClientService.createClient().

--- src/main/java/com/microtech/SmartShop/repository/ClientRepository.java
+++ src/main/java/com/microtech/SmartShop/repository/ClientRepository.java
@@ -2,6 +2,10 @@ package com.microtech.SmartShop.repository;
 
 import com.microtech.SmartShop.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Client findByUsername(String username);
+    boolean existsByEmail(String email);
 }
