21dc4f80066d5c10ead66111b818faeb33d46c96
amhine <nihadamhine@gmail.com>
2025-12-01 00:29:10 +0100

    SMAR-75 Implémenter applyLoyaltyDiscount(order).

--- src/main/java/com/microtech/SmartShop/service/impl/CommandeServiceImpl.java
+++ src/main/java/com/microtech/SmartShop/service/impl/CommandeServiceImpl.java
@@ -11,13 +11,12 @@ import com.microtech.SmartShop.mapper.CommandeMapper;
 import com.microtech.SmartShop.repository.ClientRepository;
 import com.microtech.SmartShop.repository.CommandeRepository;
 import com.microtech.SmartShop.repository.ProductRepository;
-import com.microtech.SmartShop.service.ClientService;
 import com.microtech.SmartShop.service.CommandeService;
-import com.microtech.SmartShop.service.ProductService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 @Service
 @RequiredArgsConstructor
 public class CommandeServiceImpl implements CommandeService {
@@ -27,10 +26,8 @@ public class CommandeServiceImpl implements CommandeService {
     private final CommandeMapper commandeMapper;
     private final ProductRepository productRepository;
 
-
     @Override
     public void updateStats(Client client) {
-
         long totalOrders = commandeRepository.countByClientAndStatut(client, OrderStatus.Confirmed);
         double totalSpent = commandeRepository.sumTotalTTCByClientAndStatut(client, OrderStatus.Confirmed);
 
@@ -47,7 +44,7 @@ public class CommandeServiceImpl implements CommandeService {
         clientRepository.save(client);
     }
 
-     @Override
+    @Override
     public CommandeDTO getCommande(Long id) {
         return commandeRepository.findById(id)
                 .map(commandeMapper::toDto)
@@ -60,7 +57,7 @@ public class CommandeServiceImpl implements CommandeService {
                 .orElseThrow(() -> new RuntimeException("Commande non trouvée"));
     }
 
-     @Override
+    @Override
     @Transactional
     public CommandeDTO confirmCommande(Long id) {
 
@@ -73,27 +70,46 @@ public class CommandeServiceImpl implements CommandeService {
         if (commande.getMontantRestant() > 0) {
             throw new RuntimeException("Commande non totalement payée");
         }
-
         for (OrderItem item : commande.getItems()) {
-
             Product product = item.getProduct();
-
             if (product.getStock() < item.getQuantite()) {
-
                 commande.setStatut(OrderStatus.Rejeted);
                 commandeRepository.save(commande);
-
                 throw new RuntimeException("Stock insuffisant pour : " + product.getNom());
             }
-
             product.setStock(product.getStock() - item.getQuantite());
             productRepository.save(product);
         }
-
+        applyDiscount(commande);
+        commande.setStatut(OrderStatus.Confirmed);
+        commandeRepository.save(commande);
         updateStats(commande.getClient());
 
-        commande.setStatut(OrderStatus.Confirmed);
+        return commandeMapper.toDto(commande);
+    }
 
-        return commandeMapper.toDto(commandeRepository.save(commande));
+    private void applyDiscount(Commande commande) {
+        Client client = commande.getClient();
+        CustomerTier tier = client.getCustomer();
+
+        BigDecimal sousTotal = BigDecimal.valueOf(commande.getSousTotalHT());
+        BigDecimal discountRate;
+
+        switch (tier) {
+            case Platinum -> discountRate = BigDecimal.valueOf(0.20);
+            case Gold -> discountRate = BigDecimal.valueOf(0.15);
+            case Silver -> discountRate = BigDecimal.valueOf(0.10);
+            default -> discountRate = BigDecimal.ZERO;
+        }
+
+        BigDecimal montantRemise = sousTotal.multiply(discountRate);
+        BigDecimal montantHTApresRemise = sousTotal.subtract(montantRemise);
+        BigDecimal montantTVA = montantHTApresRemise.multiply(BigDecimal.valueOf(0.2));
+        BigDecimal totalTTC = montantHTApresRemise.add(montantTVA);
+
+        commande.setMontantRemise(montantRemise.doubleValue());
+        commande.setMontantHTApresRemise(montantHTApresRemise.doubleValue());
+        commande.setMontantTVA(montantTVA.doubleValue());
+        commande.setTotalTTC(totalTTC.doubleValue());
     }
 }
